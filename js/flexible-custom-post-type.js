353b34bda382e4b751170d10a6522931edf9767a
786pe <786pe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-30 05:07:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/flexible-custom-post-type/trunk@445555 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/flexible-custom-post-type.js
+++ js/flexible-custom-post-type.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 
     jQuery('select[name=menu_icon]').live('change',function(){
         menu_icon = jQuery(this);
-        jQuery(this).next('img').attr('src', menu_icon.next('img').attr('path') + '/' + menu_icon.val());
+        menu_icon.next('span').css('backgroundImage', 'url(' + menu_icon.attr('path') + '/' + menu_icon.val() + ')');
     });
     
     jQuery('input[rel=date]').dateSelect();    
